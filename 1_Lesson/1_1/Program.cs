75c303207dcceebcef349494bba6af906b5f39d1
Kirill <zyzywin@mail.ru>
2022-06-27 02:21:03 +0300

    1_1

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
