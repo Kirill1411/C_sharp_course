d7284a828e85fea063a3b57f8030525e1b7f74a2
Kirill <zyzywin@mail.ru>
2022-06-27 04:52:53 +0300

    1_4

--- 1_Lesson/1_4/Program.cs
+++ 1_Lesson/1_4/Program.cs
@@ -1,8 +1,14 @@
 ﻿int a = int.Parse(Console.ReadLine());
 int b = -a;
-
-while (b<a+1)
-{
-Console.WriteLine(b);
-b+=1;
-}
+if (a > 0)
+    while (b < a + 1)
+    {
+        Console.WriteLine(b);
+        b += 1;
+    }
+else
+    while (a < b + 1)
+    {
+        Console.WriteLine(b);
+        b -= 1;
+    }
