370d9c7f6b7084e77e8ac608fb8bfa13e8bc3322
Kirill <zyzywin@mail.ru>
2022-06-28 00:55:22 +0300

    2_02

---
+++ 2_Lesson/2_02/Program.cs
@@ -0,0 +1,14 @@
+﻿int Maxnumber(int num)
+{
+
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+        return num / 10;
+}
+int SecondNum = new Random().Next(10, 100);
+
+Console.WriteLine(SecondNum);
+Console.WriteLine(Maxnumber(SecondNum));
