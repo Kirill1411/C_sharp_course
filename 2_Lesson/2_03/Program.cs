db1509822a1386b7d78255b0a0af65c1d3ce3d19
Kirill <zyzywin@mail.ru>
2022-06-28 01:25:54 +0300

    2_04

--- 2_Lesson/2_03/Program.cs
+++ 2_Lesson/2_03/Program.cs
@@ -1,13 +1,13 @@
 ﻿void OneOfNum(int num_1, int num_2)
 {
-if (num_1 % num_2 == 0)
-{
-Console.WriteLine($"{num_1}, {num_2} -> multiple");
-}
-else
-{
-Console.WriteLine($"{num_1}, {num_2} -> not multiple, the remainder {num_1 % num_2}");
-}
+    if (num_1 % num_2 == 0)
+    {
+        Console.WriteLine($"{num_1}, {num_2} -> multiple");
+    }
+    else
+    {
+        Console.WriteLine($"{num_1}, {num_2} -> not multiple, the remainder {num_1 % num_2}");
+    }
 }
 
 OneOfNum(16, 4);
