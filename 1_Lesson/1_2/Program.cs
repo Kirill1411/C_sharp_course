e663523f6588bc397112d9a7237a9c01802efc73
Kirill <zyzywin@mail.ru>
2022-06-27 03:04:22 +0300

    1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,37 @@
+﻿int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(day==2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(day==3)
+{
+    Console.WriteLine("Среда");
+}
+else if(day==4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(day==5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(day==6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(day==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Нет такого дня");
+}
+
+
+
