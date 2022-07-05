c72838c2493d50fa60fd6bcee90fa2840ab0dc55
Kirill <zyzywin@mail.ru>
2022-07-05 13:44:19 +0300

    3_02

---
+++ 3_Lesson/3_02/Program.cs
@@ -0,0 +1,12 @@
+﻿void Quarters(int Number)
+{
+if (Number == 1) Console.WriteLine("x > 0  y > 0");
+else if (Number == 2) Console.WriteLine("x < 0  y > 0");
+else if (Number == 3) Console.WriteLine("x < 0  y < 0");
+else if (Number == 4) Console.WriteLine("x > 0  y < 0");
+else Console.WriteLine("не верно");
+}
+
+Quarters (1);
+Quarters (4);
+Quarters (6);
