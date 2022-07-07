b021199746bae3d0ae45954a18a0f1c49d38a516
Kirill <zyzywin@mail.ru>
2022-07-08 00:50:03 +0300

    HW_4_02

--- HW_4_Lesson/4_02/Program.cs
+++ HW_4_Lesson/4_02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Sum(int num)
+{
+    int AllSum = 0;
+    while (num > 0)
+    {
+        AllSum += num % 10;
+        num /= 10;
+    }
+    return AllSum;
+}
+
+Console.WriteLine(Sum(452));
+Console.WriteLine(Sum(82));
+Console.WriteLine(Sum(9012));
