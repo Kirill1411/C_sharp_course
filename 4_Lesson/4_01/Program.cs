9287eee750bb74a456df07ff777e998aac744251
Kirill <zyzywin@mail.ru>
2022-07-08 01:10:08 +0300

    4_les

--- 4_Lesson/4_01/Program.cs
+++ 4_Lesson/4_01/Program.cs
@@ -1,11 +1,11 @@
 ﻿int SumNum(int num)
 {
-int all_sum = 0;
-for(int i = 1; i <= num; i++)
-{
-all_sum += i;
-}
-return all_sum;
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+    return all_sum;
 }
 
 Console.WriteLine(SumNum(7));
