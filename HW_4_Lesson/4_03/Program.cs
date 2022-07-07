c66cee33fbda1a802b388c9150533ea0cb20996f
Kirill <zyzywin@mail.ru>
2022-07-08 00:59:35 +0300

    HW_4_03

--- HW_4_Lesson/4_03/Program.cs
+++ HW_4_Lesson/4_03/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+/// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+void ArrayFill(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 8);
+    }
+}
+
+void ArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = new int[8];
+ArrayFill(array);
+ArrayPrint(array);
