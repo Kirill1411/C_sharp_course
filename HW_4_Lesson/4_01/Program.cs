4f53f163f20ef4ca85cb7034f312cec3c4e00b45
Kirill <zyzywin@mail.ru>
2022-07-08 00:09:08 +0300

    HW_4_01

---
+++ HW_4_Lesson/4_01/Program.cs
@@ -0,0 +1,14 @@
+﻿/// Напишите цикл, который принимает на вход
+///два числа (A и B) и возводит число A в натуральную
+///степень B.
+
+int Extent(int num1, int num2)
+{
+    int ext = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        ext *= num1;
+    }
+    return ext;
+}
+Console.WriteLine(Extent(3, 5));
