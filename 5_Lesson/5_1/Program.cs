1b3192c04e70f2787c30032abf5595b1c96f500b
Kirill <zyzywin@mail.ru>
2022-07-10 00:02:20 +0300

    5_lesson

---
+++ 5_Lesson/5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+void ChangeNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+
+    }
+    Console.WriteLine();
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+ChangeNum(arr_1);
+
+Print(arr_1);
---
+++ 5_Lesson/5_2/Program.cs
@@ -0,0 +1,35 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+void SomeNum(int[] arr, int number)
+{
+    string number_1 = "Нет";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (number == arr[i]) { number_1 = "Да"; break; }
+    }
+    Console.WriteLine($"{number}, {number_1} ");
+}
+
+int[] arr_1 = MassNums(10);
+Print(arr_1);
+SomeNum(arr_1, 8);
