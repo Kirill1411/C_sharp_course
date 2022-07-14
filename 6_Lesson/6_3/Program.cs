897b09389a50e9100ec56dec7f1445b27e80838c
Kirill <zyzywin@mail.ru>
2022-07-14 15:02:05 +0300

    6_3

--- 6_Lesson/6_3/Program.cs
+++ 6_Lesson/6_3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void FibNum(int num)
+{
+    int A = 0;
+    int B = 1;
+
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"{A} ");
+        (A, B) = (B, A + B);
+    }
+}
+
+FibNum(7);
