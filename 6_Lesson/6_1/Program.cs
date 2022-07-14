a8713c106a588374149d81d482c1a1a41a70c0ea
Kirill <zyzywin@mail.ru>
2022-07-14 14:51:28 +0300

    6_11

--- 6_Lesson/6_1/Program.cs
+++ 6_Lesson/6_1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿void Triangle(int numA, int numB, int numC)
+{
+    if (numA + numB > numC && numA + numC > numB && numB + numC > numA)
+        Console.WriteLine("Треугольник существует!");
+    else Console.WriteLine("Нет!");
+}
+
+Triangle(1, 2, 3);
