e412b315daabfaf20f73ea7a0053a6756eb6e160
Kirill <zyzywin@mail.ru>
2022-07-14 20:23:56 +0300

    HW_6_2

--- HW_6_Lesson/6_2/Program.cs
+++ HW_6_Lesson/6_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь*/
+
+int Number(int size)
+{
+    int count = 0;
+
+    for (int i = 1; i <= size; i++)
+    {
+        Console.Write($" Число {i}: ");
+        int num = int.Parse(Console.ReadLine());
+        if (num > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($" Количество чисел > 0: {Number(num)}");
