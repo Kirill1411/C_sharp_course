981903988355336b472b4e1c6fe5e2038e2a7fcb
Кирилл Зотов <107319461+Kirill1411@users.noreply.github.com>
2022-08-01 11:07:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N с помощью рекурсии.*/
+
+int Power(int M, int N)
+{
+    if (N < M) return 0;
+    return Power(M, N - 1) + N;
+}
+
+Console.WriteLine(Power(1, 15));
+Console.WriteLine(Power(4, 9));
